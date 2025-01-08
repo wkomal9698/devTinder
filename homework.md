01ed197d1db88e4e900f018628ba8af2d8d53cd4
Komal <wkomal9698@gmail.com>
2025-01-08 22:55:13 +0530

    Exploring GET, POST, DELETE API calls

--- homework.md
+++ homework.md
@@ -9,3 +9,12 @@ Install nodemon and update scripts inside package.json
 What are dependencies
 What is the use of "-g" while npm install
 Difference between caret and tilde (^ vs ~)
+
+Initialize git
+gitignore
+Create a remote git repository
+Push all code to remote origin
+Play with routes and route extensions ex. /, /hello and /hello/2
+Order of the routes matter a lot
+Install Postman app and make a workspace/collection > test API call
+Write logic to handle GET/POST/PATCH/PUT/DELETE API calls and test them on Postman
--- src/app.js
+++ src/app.js
@@ -3,21 +3,29 @@ console.log("Start of node project")
 const express = require('express')
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send("hELLO from /")
-})
+
 
 app.use('/test', (req, res) => {
     res.send("hELLO from /test")
+});
+
+// This will only handle GET call to /user
+app.get('/user', (req, res) => {
+    res.send({firstname: "Ko", lastname: "Wa"})
 })
 
-app.use('/hello', (req, res) => {
-    res.send("hELLO from /hello")
-})
+app.post('/user', (req, res) => {
+    console.log("Saving data...")
+    res.send("Data successfully saved!");
+});
 
+app.delete('/user', (req, res) => {
+    console.log("Deleting data...")
+    res.send("User successfully deleted!");
+});
 
 app.listen(3000, () => {
-    console.log("Server is successfully listening on 3000")
+    console.log("Server is successfully listening on 3000");
 });
 
 console.log("End of node project")
